0d6c9a5b6da874ea94b3e6f7ee06fc893a4c187b
Yu Qian <yuqian.124@gamil.com.com>
2019-09-08 12:49:35 +0800

    feat: 创建项目

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [new HtmlWebpackPlugin()]
+};
+
